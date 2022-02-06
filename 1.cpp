77fa79cd955b5110b8604875f431c81013e82526
handsomedonkey <33711947+handsomedonkey@users.noreply.github.com>
2022-02-06 13:55:32 +0500

    updated stuff more like ogood boy

--- 1.cpp
+++ 1.cpp
@@ -27,7 +27,7 @@ int main() {
   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
   int cur = -1, res = 1;  //set cur to -1 so the if statment doesn't get triggered on first alphabet while traversing the hum
 
-  int pos[26];    //holds position of each alphabet in cow alphabets (eg. if cow alphabets were dcab, then pos[0] = 2 representing that a (pos[0]) is in position 2)
+  int pos[26];    //holds position of each alphabet in cow alphabets (eg. if cow alphabets were dcab, then pos[0] = 2 representing that a (pos[0]) is in position 2 in cow alphabets)
   string calphs; cin >> calphs;
   for (int i = 0; i < calphs.length(); i++)
     pos[int(calphs[i]-97)] = i;             //fill up pos array with ascii magic
--- 2.cpp
+++ 2.cpp
@@ -2,29 +2,24 @@
 using namespace std;
 
 int solve(int e, int o, int n) {
-  if (e > o)  return (o * 2) + 1;
-  if (o > e) {
-    o += (e * 2); e = 0;
-    if ((o - 4) % 3 == 0) o -= ceil((o - 1) / 3.0) + 2;
+  if (e > o)  return (o * 2) + 1;   //if e>o than we can just treat it as e=o since our extra e's cannot make an odd but can be put with other e's to get rid of themselves
+  if (o > e) {                      //if o>e convert some o's to e's   
+    o += (e * 2); e = 0;          
+    if ((o - 4) % 3 == 0) o -= ceil((o - 1) / 3.0) + 2;   
     else o -= ceil((o - 1) / 3.0);                          
   }
-  return o+e;
+  return o+e;                       //return o+e which would be n if o==e and first two 'if' statements were passed, or o if the second 'if' statement was true
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0); cout.tie(0);
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-  int n;
-  cin >> n;
+  int n; cin >> n;
   int e = 0, o = 0;
   for (int i = 0; i < n; i++) {
-    int k;
-    cin >> k;
-    if (k % 2 == 0)
-      ++e;
-    else
-      ++o;
+    int k; cin >> k;
+    if (k % 2 == 0) ++e;
+    else ++o;
   }
   cout << solve(e, o, n);
 }
